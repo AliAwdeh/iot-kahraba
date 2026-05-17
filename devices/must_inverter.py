dfa944423ab8d346c30c214cf342a41b65fc23aa
Ali Awdeh <ali.i.awdeh@gmail.com>
2026-05-17 23:49:08 +0300

    45 volts test 3

--- devices/must_inverter.py
+++ devices/must_inverter.py
@@ -314,13 +314,14 @@ class MustInverterReader:
         if data.get("pv_warning_message", 0) not in [0, None]:
             return "PV_WARNING"
 
-        battery_voltage = data.get("battery_voltage")
+        battery_voltage = None
+        inverter_data = data.get("inverter_data")
+
+        if isinstance(inverter_data, dict):
+            battery_voltage = inverter_data.get("inverter_battery_voltage")
 
         if battery_voltage is None:
-            inverter_data = data.get("inverter_data")
-
-            if isinstance(inverter_data, dict):
-                battery_voltage = inverter_data.get("inverter_battery_voltage")
+            battery_voltage = data.get("battery_voltage")
 
         if (
             is_main_inverter
@@ -596,11 +597,7 @@ class MustInverterReader:
 
         if inverter_data:
             payload.update({
-                "battery_voltage": (
-                    payload["battery_voltage"]
-                    if payload["battery_voltage"] is not None
-                    else inverter_data["inverter_battery_voltage"]
-                ),
+                "battery_voltage": inverter_data["inverter_battery_voltage"],
                 "inverter_voltage": inverter_data["inverter_voltage"],
                 "grid_voltage": inverter_data["grid_voltage"],
                 "bus_voltage": inverter_data["bus_voltage"],
